d2a5d5990f213976a73e1ce0076ff686fc17e72c
Julien Tanay <julien.tanay@gmail.com>
2021-04-01 14:55:25 +0200

    Get pranked

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { DefaultSeo } from "next-seo";
+
+import "tailwindcss/tailwind.css";
+
+const App = ({ Component, pageProps }) => (
+  <>
+    <DefaultSeo
+      description="Reboot As A Service"
+      openGraph={{
+        type: "website",
+        locale: "en_US",
+        url: "https://reboot.djiit.dev",
+        site_name: "Reboot",
+        images: [
+          {
+            url: "https://reboot.djiit.dev/summary.png",
+            width: 1382,
+            height: 728,
+            alt: "Reboot",
+          },
+        ],
+      }}
+      twitter={{
+        handle: "@Djiit",
+        site: "@Djiit",
+        cardType: "summary_large_image",
+      }}
+    />
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
